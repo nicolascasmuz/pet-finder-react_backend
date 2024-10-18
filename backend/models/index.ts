b53721cd1154ba4a45a640ef0e16e99c63bdd9b5
Nicolás Casmuz <nicolascasmuz@gmail.com>
2024-10-18 11:58:03 -0300

    commit-001

--- backend/models/index.ts
+++ backend/models/index.ts
@@ -1,3 +1,5 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(process.env.ELEPHANTSQL_URL);
+export const sequelize = new Sequelize(
+  "postgres://cadvdiaj:MHYYhynih4WSNCntRyBufnvnTMSE35F6@topsy.db.elephantsql.com/cadvdiaj"
+);
